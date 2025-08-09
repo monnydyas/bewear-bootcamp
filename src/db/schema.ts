0aa5bb1ea2739936da1f0192d87a0e46defc1659
monnydyas <monnydyas@gmail.com>
2025-08-09 12:10:21 -0300

    feat: add sign up

--- src/db/schema.ts
+++ src/db/schema.ts
@@ -24,7 +24,7 @@ export const userTable = pgTable("user", {
     .notNull(),
 });
 
-export const session = pgTable("session", {
+export const sessionTable = pgTable("session", {
   id: text("id").primaryKey(),
   expiresAt: timestamp("expires_at").notNull(),
   token: text("token").notNull().unique(),
