a6b0f78eed447e58ca65b018ac75e26a1da340ed
monnydyas <monnydyas@gmail.com>
2025-09-07 18:46:52 -0300

    feat: add address form

--- src/components/common/cart.tsx
+++ src/components/common/cart.tsx
@@ -1,6 +1,7 @@
 "use client";
 
 import { ShoppingBasketIcon } from "lucide-react";
+import Link from "next/link";
 
 import { formatCentsToBRL } from "@/helpers/money";
 import { useCart } from "@/hooks/queries/use-cart";
@@ -76,7 +77,9 @@ export const Cart = () => {
                 <p>{formatCentsToBRL(cart?.totalPriceInCents ?? 0)}</p>
               </div>
 
-              <Button className="mt-5 rounded-full">Finalizar compra</Button>
+              <Button className="mt-5 rounded-full" asChild>
+                <Link href="/cart/identification">Finalizar a compra</Link>
+              </Button>
             </div>
           )}
         </div>
